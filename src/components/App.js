b9168d92c3d4b6279f8ae4bbe456eff7140829fc
Dandazine <ddaneshfar@my.bcit.ca>
2022-08-24 13:46:06 -0700

    HTML prototype done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,40 @@
 
 function App() {
+
+  function keyCreate (){
+    let numArray = [];
+    for (let i =0; i < 10; i++){
+     numArray.push(<button key={i}>{i}</button>)
+    }
+    return(
+      numArray
+    )
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <section className="calculator">
+      <div className="display">
+        <span>(0)</span> 0
+      </div>
+        
+      {/* Make this entire thing a grid ??  */}
+      <div className="op-buttons">
+        <button>*</button>
+        <button>/</button>
+        <button>+</button>
+        <button>-</button>
+        <button>%</button>
+        <button>√</button>
+        <button>C</button>
+        <button>AC</button>
+      </div>
+
+      <div className="nums">
+        {keyCreate()}
+        <button>.</button>
+        <button>=</button>
+      </div>
+    </section>
   );
 }
 
