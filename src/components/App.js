51e8863f224ca5df94d6dca50ac3753b41fac26f
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-08-24 14:51:28 -0700

    First version of Calculator app created

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,81 @@
+import {useState} from 'react';
 
 function App() {
+  const operators = ['.', '-', '+', '*', '/'];
+  const [calculate, setCalculate] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const updatedCalculations = value => {
+    if (operators.includes(value) && calculate === '') {
+      return;
+    } else if(operators.includes(value) && operators.includes(calculate.slice(-1))) {
+      return;
+    }
+
+    setCalculate(calculate + value);
+
+    if(!operators.includes(value)) {
+      setAnswer(eval(calculate + value).toString())
+    }
+  }
 
   function keyCreate (){
     let numArray = [];
     for (let i =0; i < 10; i++){
-     numArray.push(<button key={i}>{i}</button>)
+     numArray.push(<button onClick={() => updatedCalculations(i.toString())} key={i}>{i}</button>)
     }
     return(
       numArray
     )
   }
 
+  function giveAnswer (){
+    setCalculate(eval(calculate).toString());
+  }
+
+  function deleteNumber (){
+    if (calculate === '') {
+      return;
+    } 
+
+    const value = calculate.slice(0, -1)
+
+    setCalculate(value);
+  }
+
+  function deleteFullCalculation (){
+    if (calculate === '') {
+      return;
+    } 
+
+    const value = calculate.slice(0, 0)
+
+    setCalculate(value);
+  }
+
   return (
     <section className="calculator">
       <div className="display">
-        <span>(0)</span> 0
+        {answer ? <span>({answer})</span> : ''} 
+        {calculate || "0"}
       </div>
         
       {/* Make this entire thing a grid ??  */}
       <div className="op-buttons">
-        <button>*</button>
-        <button>/</button>
-        <button>+</button>
-        <button>-</button>
-        <button>%</button>
-        <button>√</button>
-        <button>C</button>
-        <button>AC</button>
+        <button onClick={() => updatedCalculations('*')}>*</button>
+        <button onClick={() => updatedCalculations('/')}>/</button>
+        <button onClick={() => updatedCalculations('+')}>+</button>
+        <button onClick={() => updatedCalculations('-')}>-</button>
+        <button onClick={() => updatedCalculations('%')}>%</button>
+        <button onClick={() => updatedCalculations('√')}>√</button>
+        <button onClick={deleteNumber}>C</button>
+        <button onClick={deleteFullCalculation}>AC</button>
       </div>
 
       <div className="nums">
         {keyCreate()}
-        <button>.</button>
-        <button>=</button>
+        <button onClick={() => updatedCalculations('.')}>.</button>
+        <button onClick={giveAnswer}>=</button>
       </div>
     </section>
   );
